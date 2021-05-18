de00028298331c56c0763e2f46bc7be6758ec687
Kyle Lin <lyhcode@gmail.com>
2021-05-18 23:06:30 +0800

    update ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,41 @@
 module.exports = {
-  apps : [{
-    name   : 'pm2-ecosystem-demo',
-    script : './bin/www',
-    watch  : true,
+  apps: [{
+    name: 'pm2-ecosystem-demo',
+    script: './bin/www',
+    watch: true,
     env: {
-      NODE_ENV : 'development',
-      PORT     : process.env.PORT | '3001',
+      NODE_ENV: 'development',
+      PORT: process.env.PORT | '3000',
     },
     env_staging: {
-      NODE_ENV : 'production',
-      PORT     : process.env.PORT | '8001',
+      NODE_ENV: 'production',
+      PORT: process.env.PORT | '8000',
     },
     env_production: {
-      NODE_ENV : 'production',
-      PORT     : process.env.PORT | '8001',
+      NODE_ENV: 'production',
+      PORT: process.env.PORT | '8000',
     }
   }],
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+  deploy: {
+    development: {
+      user: 'deploy',
+      host: 'linode2.trunksys.com',
+      ref: 'origin/develop',
+      repo: 'git@github.com:monosparta/pm2-ecosystem-demo.git',
+      path: '/home/deploy/pm2-ecosystem-demo',
+      'pre-setup': 'git --version',
+      'post-setup': 'ls -al',
+      'pre-deploy-local': "echo 'This is a local executed command'",
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js',
+    },
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
